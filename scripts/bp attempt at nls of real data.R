bab8d5864f0d62ceae73484f38f900a87122c6f4
Bill <wlperry@gmail.com>
2021-06-29 11:41:29 -0500

    fix row header

--- scripts/bp attempt at nls of real data.R
+++ scripts/bp attempt at nls of real data.R
@@ -1,1 +1,22 @@
 library(tidyverse)
+library(janitor)
+
+# read in file
+decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
+
+# make row a number
+decomp.df <- decomp.df %>% 
+  mutate(row_no = case_when(
+   row == "A" ~ 1,
+   row == "B" ~ 2,
+   row == "C" ~ 3,
+   row == "D" ~ 4,
+   row == "E" ~ 5,
+   row == "F" ~ 6,
+   row == "G" ~ 7,
+   row == "H" ~ 8,
+   row == "I" ~ 9,
+   row == "J" ~ 10,
+    TRUE ~ 99999
+  ) )
+
