ff787450295bb2fd79e305f2956ec825236d4eba
Ryan Meyer <RyanMeyer@MacBook-Pro-7.local>
2022-09-07 12:58:27 -0500

    removed 1 line

--- scripts/02 Nitrogen Analysis.R
+++ scripts/02 Nitrogen Analysis.R
@@ -1,117 +1,117 @@
-# LIBRARIES ----
-library(tidyverse)
-library(janitor)
-library(purrr)
-library(broom)
-library(car)
-library(emmeans)
-library(patchwork)
-library(multcompView)
-library(lsmeans)
-
-# READ IN THE DATA ----
-
-# decomp
-decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
-
-# n data 
-n_data.df <- read.csv("Data/Decomp/n with data.csv") %>%
-  clean_names() %>%
-  remove_empty(which = c("cols", "rows"))
-
-
-# CLEAN DATA TO JOIN ----
-
-# rename and make factors
-decomp.df <- decomp.df %>%
-  rename(sample = bag_no) %>%
-  mutate(sample = as.factor(sample))
-
-# make factor
-n_data.df <- n_data.df %>%
-  mutate(sample = as.factor(sample))
-
-# JOIN FILES ----
-
-# full join 
-full.df <- full_join(decomp.df, n_data.df, by = "sample")
-
-# remove old dataframes
-rm(decomp.df, n_data.df)
-
-# CLEAN FULL DATAFRAME ----
-
-# remove na from data we didn't send 
-full.df <- full.df %>%
-  na.omit()
-
-# make row a number
-full.df <- full.df %>% 
-  mutate(row = case_when(
-    row == "A" ~ 1,
-    row == "B" ~ 2,
-    row == "C" ~ 3,
-    row == "D" ~ 4,
-    row == "E" ~ 5,
-    row == "F" ~ 6,
-    row == "G" ~ 7,
-    row == "H" ~ 8,
-    row == "I" ~ 9,
-    row == "J" ~ 10,
-    TRUE ~ 99999
-  ) )
-
-# CALCS FOR PROP N REMAINING ----
-
-# first need day 0 porportion of n * initial wt for how much n in initial weight
-# calculate proportion n 
-full.df <- full.df %>% 
-  mutate(prop_n = percent_n / 100)
-
-# now we need day 0 and by species 
-initial_prop_n.df <- full.df %>%
-  filter(days == 0) %>%
-  group_by(spp) %>%
-  summarize(initial_prop_n = mean(prop_n))
-
-# we then multiply each sample by the initial prop n to get the initial n for every sample
-full.df <- full.df %>%
-  mutate(initial_prop_n = case_when(
-    spp == "PC" ~ initial_wt_g * 0.01872000,
-    spp == "GM_PC" ~ initial_wt_g * 0.01595455,
-    spp == "AR" ~ initial_wt_g * 0.01711000,
-    spp == "CR" ~ initial_wt_g * 0.01439000
-  ))
-
-# now we take collected weight and multiply by the collected prop n to get collected n
-full.df <- full.df %>%
-  mutate(collected_prop_n = coll_wt_g * prop_n)
-
-# finally divide initial by collected to get prop n remaining then * 100 for pct
-full.df <- full.df %>%
-  mutate(prop_n_remain = collected_prop_n / initial_prop_n) %>%
-  mutate(pct_n_remain = prop_n_remain * 100)
-
-# graph it to see how it looks
-full.df %>%
-  ggplot(mapping = aes(days, pct_n_remain, color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  theme_classic()
-
-# gonna remove those problem days 
-unique(full.df$days)
-
-full.df <- full.df %>%
-  filter(days == 0 | days == 14 | days == 35 | days == 63)
-
-# graph it to see how it looks
-full.df %>%
-  ggplot(mapping = aes(log(days+1), log(pct_n_remain), color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  geom_smooth(method="lm", se=F)
-  theme_classic()
+# # LIBRARIES ----
+# library(tidyverse)
+# library(janitor)
+# library(purrr)
+# library(broom)
+# library(car)
+# library(emmeans)
+# library(patchwork)
+# library(multcompView)
+# library(lsmeans)
+# 
+# # READ IN THE DATA ----
+# 
+# # decomp
+# decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
+# 
+# # n data 
+# n_data.df <- read.csv("Data/Decomp/n with data.csv") %>%
+#   clean_names() %>%
+#   remove_empty(which = c("cols", "rows"))
+# 
+# 
+# # CLEAN DATA TO JOIN ----
+# 
+# # rename and make factors
+# decomp.df <- decomp.df %>%
+#   rename(sample = bag_no) %>%
+#   mutate(sample = as.factor(sample))
+# 
+# # make factor
+# n_data.df <- n_data.df %>%
+#   mutate(sample = as.factor(sample))
+# 
+# # JOIN FILES ----
+# 
+# # full join 
+# full.df <- full_join(decomp.df, n_data.df, by = "sample")
+# 
+# # remove old dataframes
+# rm(decomp.df, n_data.df)
+# 
+# # CLEAN FULL DATAFRAME ----
+# 
+# # remove na from data we didn't send 
+# full.df <- full.df %>%
+#   na.omit()
+# 
+# # make row a number
+# full.df <- full.df %>% 
+#   mutate(row = case_when(
+#     row == "A" ~ 1,
+#     row == "B" ~ 2,
+#     row == "C" ~ 3,
+#     row == "D" ~ 4,
+#     row == "E" ~ 5,
+#     row == "F" ~ 6,
+#     row == "G" ~ 7,
+#     row == "H" ~ 8,
+#     row == "I" ~ 9,
+#     row == "J" ~ 10,
+#     TRUE ~ 99999
+#   ) )
+# 
+# # CALCS FOR PROP N REMAINING ----
+# 
+# # first need day 0 porportion of n * initial wt for how much n in initial weight
+# # calculate proportion n 
+# full.df <- full.df %>% 
+#   mutate(prop_n = percent_n / 100)
+# 
+# # now we need day 0 and by species 
+# initial_prop_n.df <- full.df %>%
+#   filter(days == 0) %>%
+#   group_by(spp) %>%
+#   summarize(initial_prop_n = mean(prop_n))
+# 
+# # we then multiply each sample by the initial prop n to get the initial n for every sample
+# full.df <- full.df %>%
+#   mutate(initial_prop_n = case_when(
+#     spp == "PC" ~ initial_wt_g * 0.01872000,
+#     spp == "GM_PC" ~ initial_wt_g * 0.01595455,
+#     spp == "AR" ~ initial_wt_g * 0.01711000,
+#     spp == "CR" ~ initial_wt_g * 0.01439000
+#   ))
+# 
+# # now we take collected weight and multiply by the collected prop n to get collected n
+# full.df <- full.df %>%
+#   mutate(collected_prop_n = coll_wt_g * prop_n)
+# 
+# # finally divide initial by collected to get prop n remaining then * 100 for pct
+# full.df <- full.df %>%
+#   mutate(prop_n_remain = collected_prop_n / initial_prop_n) %>%
+#   mutate(pct_n_remain = prop_n_remain * 100)
+# 
+# # graph it to see how it looks
+# full.df %>%
+#   ggplot(mapping = aes(days, pct_n_remain, color = spp)) +
+#   stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
+#   stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
+#   theme_classic()
+# 
+# # gonna remove those problem days 
+# unique(full.df$days)
+# 
+# full.df <- full.df %>%
+#   filter(days == 0 | days == 14 | days == 35 | days == 63)
+# 
+# # graph it to see how it looks
+# full.df %>%
+#   ggplot(mapping = aes(log(days+1), log(pct_n_remain), color = spp)) +
+#   stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
+#   stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
+#   geom_smooth(method="lm", se=F)
+#   theme_classic()
 
 # LIBRARIES ----
 library(tidyverse)
@@ -126,7 +126,6 @@ library(lsmeans)
   
 # READ IN THE DATA ----
 full.df <- read_csv("data/cleaned/clean nitrogen.csv")
-
 
 # NONLINEAR MODELS ----
 
