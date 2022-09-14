86def964c82b88477170085c79828482491bec57
Bill <wlperry@gmail.com>
2022-09-14 16:28:38 -0500

    log models

---
+++ scripts/02_total_biomass_linear approach.R
@@ -0,0 +1,80 @@
+# LIBRARIES ----
+library(tidyverse)
+library(janitor)
+library(purrr)
+library(broom)
+library(car)
+library(emmeans)
+library(patchwork)
+
+# READ IN THE DECOMP DATA ----
+# read in file 
+decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
+
+# CLEAN DECOMP DATA ----
+
+# make row a number
+decomp.df <- decomp.df %>% 
+  mutate(row_no = case_when(
+    row == "A" ~ 1,
+    row == "B" ~ 2,
+    row == "C" ~ 3,
+    row == "D" ~ 4,
+    row == "E" ~ 5,
+    row == "F" ~ 6,
+    row == "G" ~ 7,
+    row == "H" ~ 8,
+    row == "I" ~ 9,
+    row == "J" ~ 10,
+    TRUE ~ 99999
+  ) )
+
+# math modifications to get pct mass remaining  
+decomp.df <- decomp.df %>% 
+  mutate(pct_mass_remain = ((coll_wt_g/ initial_wt_g)*100)) %>% 
+  mutate(soil_block = as.factor(soil_block),
+         spp = as.factor(spp))
+
+# remove missing values 
+decomp.df <- decomp.df %>% 
+  filter(!is.na(initial_wt_g))
+
+
+# correct for day 0 mass remaining  
+corr_t0.df <- decomp.df %>%
+  filter(days == 0) %>%
+  group_by(spp) %>%
+  summarize(pct_mass_remain = 100 - mean(pct_mass_remain, na.rm=TRUE))
+
+# correct mass remaining from time 0 
+# Do we adjust all masses to this correction 
+decomp.df <- decomp.df %>%
+  mutate(pct_mass_remain_corr = case_when(
+    spp == "PC"    & days == 0  ~ pct_mass_remain + 2.0582047,
+    spp == "GM_PC" & days == 0  ~ pct_mass_remain + -0.2269507,
+    spp == "AR"    & days == 0  ~ pct_mass_remain + 7.1806311,
+    spp == "CR"    & days == 0  ~ pct_mass_remain + 7.4213231,
+    TRUE ~ pct_mass_remain
+  ))
+
+# PLOT TO LOOK AT THE NEGATIVE EXPONENTIALS ----
+decomp.df %>%
+  mutate(spp = as.factor(spp)) %>% 
+  mutate(spp = fct_reorder2(spp, days, pct_mass_remain)) %>%
+  ggplot(mapping = aes(days, pct_mass_remain, color = spp)) +
+  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
+  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
+  theme_classic()
+
+pc.df <- decomp.df %>% 
+  filter(spp=="PC") %>% 
+  mutate(ln_pct_mass = log(pct_mass_remain +1),
+         ln_day = log(days +1))
+
+pc.df %>% 
+  ggplot(aes(days, ln_pct_mass)) +
+  geom_point() +
+  geom_smooth(method="lm")
+
+
+
