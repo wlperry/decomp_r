217dc77e5b07c445d6270c0875bd55eb5c698a82
Ryan Meyer <RyanMeyer@MacBook-Pro-8.local>
2022-09-22 09:46:57 -0500

    Deleted unused files, cleaned up 01 import and clean, moved cleaned files from import and clean to output folder.

--- scripts/01 file import and clean.R
+++ scripts/01 file import and clean.R
@@ -1,10 +1,13 @@
-# LIBRARIES
+# LIBRARIES ----
 library(tidyverse)
 library(janitor)
 
 # TOTAL BIOMASS ----
-# read in file
+
+# read in file ----
 decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
+
+# cleaning ----
 
 # make row a number
 decomp.df <- decomp.df %>% 
@@ -22,24 +25,26 @@ decomp.df <- decomp.df %>%
     TRUE ~ 99999
   ) )
 
-# math modifications to get pct mass remaining  -----
+# calcs ----
+
+# math modifications to get pct mass remaining  
 decomp.df <- decomp.df %>% 
   mutate(pct_mass_remain = ((coll_wt_g/ initial_wt_g)*100)) %>% 
   mutate(soil_block = as.factor(soil_block),
          spp = as.factor(spp))
 
-# remove missing values -----
+# remove missing values 
 decomp.df <- decomp.df %>% 
   filter(!is.na(initial_wt_g))
 
-# correct for day 0 mass remaining  -----
+# correct for day 0 mass remaining  
 corr_t0.df <- decomp.df %>%
   filter(days == 0) %>%
   group_by(spp) %>%
   summarize(pct_mass_remain = 100 - mean(pct_mass_remain, na.rm=TRUE))
 
-# correct mass remaining from time 0 -----
-# Do we adjust all masses to this correction ----
+# correct mass remaining from time 0 
+# Do we adjust all masses to this correction 
 decomp.df <- decomp.df %>%
   mutate(pct_mass_remain_corr = case_when(
     spp == "PC"    & days == 0  ~ pct_mass_remain + 2.0582047,
@@ -53,29 +58,21 @@ decomp.df <- decomp.df %>%
 unique(decomp.df$days)
 #  0  7 14 21 28 35 49 63 84 NA
 
+# NITROGEN AND CARBON ----
 
+# read in data ----
 
-# save as csv
-write_csv(decomp.df, file = "data/cleaned/clean total biomass.csv")
-
-
-# NITROGEN ----
-
-# read in the data
-
-# decomp
-decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
+nutrients.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
 
 # n data 
 n_data.df <- read.csv("Data/Decomp/n with data.csv") %>%
   clean_names() %>%
   remove_empty(which = c("cols", "rows"))
 
-
-# clean the data
+# clean the data ----
 
 # rename and make factors
-decomp.df <- decomp.df %>%
+nutrients.df <- nutrients.df %>%
   rename(sample = bag_no) %>%
   mutate(sample = as.factor(sample))
 
@@ -86,19 +83,19 @@ n_data.df <- n_data.df %>%
 # join files
 
 # full join 
-full.df <- full_join(decomp.df, n_data.df, by = "sample")
+full_nutrients.df <- full_join(nutrients.df, n_data.df, by = "sample")
 
 # remove old dataframes
-rm(decomp.df, n_data.df)
+rm(nutrients.df, n_data.df)
 
 # clean full dataframe
 
 # remove na from data we didn't send 
-full.df <- full.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   na.omit()
 
 # make row a number
-full.df <- full.df %>% 
+full_nutrients.df <- full_nutrients.df %>% 
   mutate(row = case_when(
     row == "A" ~ 1,
     row == "B" ~ 2,
@@ -113,21 +110,21 @@ full.df <- full.df %>%
     TRUE ~ 99999
   ) )
 
-# CALCS FOR PROP N REMAINING 
+# calcs for prop n remaining ----
 
 # first need day 0 porportion of n * initial wt for how much n in initial weight
 # calculate proportion n 
-full.df <- full.df %>% 
+full_nutrients.df <- full_nutrients.df %>% 
   mutate(prop_n = percent_n / 100)
 
 # now we need day 0 and by species 
-initial_prop_n.df <- full.df %>%
+initial_prop_n.df <- full_nutrients.df %>%
   filter(days == 0) %>%
   group_by(spp) %>%
   summarize(initial_prop_n = mean(prop_n))
 
 # we then multiply each sample by the initial prop n to get the initial n for every sample
-full.df <- full.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(initial_prop_n = case_when(
     spp == "PC" ~ initial_wt_g * 0.01872000,
     spp == "GM_PC" ~ initial_wt_g * 0.01595455,
@@ -136,105 +133,29 @@ full.df <- full.df %>%
   ))
 
 # now we take collected weight and multiply by the collected prop n to get collected n
-full.df <- full.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(collected_prop_n = coll_wt_g * prop_n)
 
 # finally divide initial by collected to get prop n remaining then * 100 for pct
-full.df <- full.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(prop_n_remain = collected_prop_n / initial_prop_n) %>%
   mutate(pct_n_remain = prop_n_remain * 100)
 
-# graph it to see how it looks
-full.df %>%
-  ggplot(mapping = aes(days, pct_n_remain, color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  theme_classic()
+# calcs for prop c remaining ----
 
-# gonna remove those problem days 
-unique(full.df$days)
-
-full.df <- full.df %>%
-  filter(days == 0 | days == 14 | days == 35 | days == 63)
-
-# graph it to see how it looks
-full.df %>%
-  ggplot(mapping = aes(log(days+1), log(pct_n_remain), color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  geom_smooth(method="lm", se=F)
-theme_classic()
-
-# save as csv
-write_csv(full.df, file = "data/cleaned/clean nitrogen.csv")
-
-
-# CARBON ----
-
-# READ IN THE DATA 
-
-# decomp
-decomp.df <- read_csv("data/decomp_biomass.csv") %>% clean_names()
-
-# n data 
-n_data.df <- read.csv("Data/Decomp/n with data.csv") %>%
-  clean_names() %>%
-  remove_empty(which = c("cols", "rows"))
-
-
-# CLEAN DATA TO JOIN 
-
-# rename and make factors
-decomp.df <- decomp.df %>%
-  rename(sample = bag_no) %>%
-  mutate(sample = as.factor(sample))
-
-# make factor
-n_data.df <- n_data.df %>%
-  mutate(sample = as.factor(sample))
-
-# JOIN FILES 
-
-# full join 
-carbon.df <- full_join(decomp.df, n_data.df, by = "sample")
-
-# CLEAN FULL DATAFRAME 
-
-# remove na from data we didn't send 
-carbon.df <- carbon.df %>%
-  na.omit()
-
-# make row a number
-carbon.df <- carbon.df %>% 
-  mutate(row = case_when(
-    row == "A" ~ 1,
-    row == "B" ~ 2,
-    row == "C" ~ 3,
-    row == "D" ~ 4,
-    row == "E" ~ 5,
-    row == "F" ~ 6,
-    row == "G" ~ 7,
-    row == "H" ~ 8,
-    row == "I" ~ 9,
-    row == "J" ~ 10,
-    TRUE ~ 99999
-  ) )
-
-# C IN BIOMASS 
-
-# first need day 0 porportion of n * initial wt for how much n in initial weight
+# first need day 0 porportion of c * initial wt for how much c in initial weight
 # calculate proportion n 
-carbon.df <- carbon.df %>% 
+full_nutrients.df <- full_nutrients.df %>% 
   mutate(prop_c = percent_c / 100)
 
 # now we need day 0 and by species 
-initial_prop_c.df <- carbon.df %>%
+initial_prop_c.df <- full_nutrients.df %>%
   filter(days == 0) %>%
   group_by(spp) %>%
   summarize(initial_prop_c = mean(prop_c))
 
 # we then multiply each sample by the initial prop n to get the initial n for every sample
-carbon.df <- carbon.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(initial_prop_c = case_when(
     spp == "PC" ~ initial_wt_g * 0.4549000,
     spp == "GM_PC" ~ initial_wt_g * 0.4313636,
@@ -243,36 +164,27 @@ carbon.df <- carbon.df %>%
   ))
 
 # now we take collected weight and multiply by the collected prop n to get collected n
-carbon.df <- carbon.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(collected_prop_c = coll_wt_g * prop_c)
 
 # finally divide initial by collected to get prop n remaining then * 100 for pct
-carbon.df <- carbon.df %>%
+full_nutrients.df <- full_nutrients.df %>%
   mutate(prop_c_remain = collected_prop_c / initial_prop_c) %>%
-  mutate(pct_c_remaing = prop_c_remain * 100)
-
-# graph it to see how it looks
-carbon.df %>%
-  ggplot(mapping = aes(days, pct_c_remaing, color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  theme_classic()
-
-# gonna have to get rid of those weird days that got sampled
-unique(carbon.df$days)
-
-carbon.df <- carbon.df %>%
-  filter(days == 0 | days == 14 | days == 35 | days == 63)
-
-# plot it again to see if I removed the correct days, yep looks good
-carbon.df %>%
-  ggplot(mapping = aes(days, pct_c_remaing, color = spp)) +
-  stat_summary(fun = mean, na.rm = TRUE, geom = "point") +
-  stat_summary(fun.data = mean_se, na.rm = TRUE, geom = "line") +
-  theme_classic()
-
-# save as csc
-write_csv(carbon.df, file = "data/cleaned/clean carbon.csv")
+  mutate(pct_c_remain = prop_c_remain * 100)
 
 
-test 1
+# SAVE FILES TO OUTPUT ----
+
+# decomp ----
+write_csv(decomp.df, file = 
+            "output/final files from import and k calcs/from import and clean/total biomass.csv")
+
+# nitrogen and carbon
+write_csv(full_nutrients.df, file = 
+            "output/final files from import and k calcs/from import and clean/nutrients.csv")
+
+
+
+
+
+
